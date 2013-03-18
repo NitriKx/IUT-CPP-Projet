f703cee1c67969fb614f077c091d18d7f80d0ddc
NitriKx <nitrikxisback@gmail.com>
2013-03-18 20:46:27 +0100

    Création d'un test dans le main

--- Projet-CPP/src/EntryPoint.cpp
+++ Projet-CPP/src/EntryPoint.cpp
@@ -16,11 +16,11 @@ int main()
 	
 	Monde *monde = new Monde();
 
-	Element *e1 = new Element();
-	Element *e2 = new Element();
+	Element *e1 = new Element("E1", Position(1,1));
+	Element *e2 = new Element("E2", Position(3,3));
 
-	Mobile *m1 = new Mobile();
-	Mobile *m1 = new Mobile();
+	Mobile *m1 = new Mobile("M1", Position(2,2), 2,3);
+	Mobile *m2 = new Mobile("M1", Position(2,1), 1,3);
 
 	return 0;
 }
