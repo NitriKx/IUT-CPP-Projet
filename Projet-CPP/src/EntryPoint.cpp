c8db2d5806f271bdd5159dfdc89a31b660abd5ee
NitriKx <nitrikxisback@gmail.com>
2013-03-18 20:49:39 +0100

    Fin de la création des données de test.

--- Projet-CPP/src/EntryPoint.cpp
+++ Projet-CPP/src/EntryPoint.cpp
@@ -19,8 +19,15 @@ int main()
 	Element *e1 = new Element("E1", Position(1,1));
 	Element *e2 = new Element("E2", Position(3,3));
 
-	Mobile *m1 = new Mobile("M1", Position(2,2), 2,3);
-	Mobile *m2 = new Mobile("M1", Position(2,1), 1,3);
+	Mobile *m1 = new Mobile("M1", Position(2,2), 100, 5, 2000, 2, 3);
+	Mobile *m2 = new Mobile("M2", Position(2,0), 100, 5, 2000, 2, 3);
+
+	monde->ajouterElement(e1->getPosition(), e1);
+	monde->ajouterElement(e2->getPosition(), e2);
+	monde->ajouterElement(m1->getPosition(), m1);
+	monde->ajouterElement(m2->getPosition(), m2);
+
+
 
 	return 0;
 }
