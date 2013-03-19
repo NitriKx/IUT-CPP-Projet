dc9028a1aa067b36bfe7f3157a6c292538866823
fleyer <manchado.romain@gmail.com>
2013-03-19 08:50:08 +0100

    Ajout classe gaulois gauloise

---
+++ Projet-CPP/src/Gaulois.cpp
@@ -0,0 +1,22 @@
+#include "Gaulois.h"
+
+
+Gaulois::Gaulois(void)
+{
+}
+
+Gaulois::Gaulois(string nom, Position pos, unsigned int _ptVie, unsigned int _espVie, unsigned int _forceCombat, unsigned int _vitesse, unsigned int _vision) : Humain( nom, pos, _ptVie, _espVie, _forceCombat, _vitesse, _vision)
+{
+}
+
+
+Gaulois::~Gaulois(void)
+{
+}
+
+char Gaulois::getAffich() {
+	return 'G';
+}
+COLORS Gaulois::getCouleur() {	
+	return BLUE;
+}
--- Projet-CPP/src/Humain.h
+++ Projet-CPP/src/Humain.h
@@ -8,9 +8,10 @@ class Humain :
 {
 
 private :
-	int vie; 
+
 public:
 	Humain(void);
+	Humain(string nom, Position pos, unsigned int _ptVie, unsigned int _espVie, unsigned int _forceCombat, unsigned int _vitesse, unsigned int _vision);
 	~Humain(void);
 
 	char getAffich();
